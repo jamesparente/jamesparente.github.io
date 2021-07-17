cfaa5838dc8894567046ec207b0f44dee112cf74
jamesparente <85665555+jamesparente@users.noreply.github.com>
2021-07-17 16:06:23 -0400

    Add files via upload

--- islt_7310/js/slide_show.js
+++ islt_7310/js/slide_show.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 	
 	// the function for running the slide show	
 	var runSlideShow = function() {   
-        $("#caption").fadeOut(3000);
-        $("#slide").fadeOut(3000,
+        $("#caption").fadeOut(1000);
+        $("#slide").fadeOut(1000,
         	function () {
        	     	if (nextSlide.next().length == 0) {
 					nextSlide = $("#slides img:first-child");
@@ -17,14 +17,14 @@ $(document).ready(function() {
 				}
 				nextSlideSource = nextSlide.attr("src");
 				nextCaption = nextSlide.attr("alt"); 
-				$("#slide").attr("src", nextSlideSource).fadeIn(3000);					
-				$("#caption").text(nextCaption).fadeIn(3000);
+				$("#slide").attr("src", nextSlideSource).fadeIn(1000);					
+				$("#caption").text(nextCaption).fadeIn(1000);
 			}
 		)
 	}
 	
 	// start slide show
-    var timer1 = setInterval(runSlideShow, 3000);
+    var timer1 = setInterval(runSlideShow, 1000);
     
     // start and stop the slide show
 	// but the toggle event method has been removed from jQuery 1.9
